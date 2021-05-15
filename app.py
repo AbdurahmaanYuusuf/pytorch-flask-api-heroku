c3937ee3b673600fb09971c7210a73b23e4120cf
AbdurahmaanYuusuf <55019409+AbdurahmaanYuusuf@users.noreply.github.com>
2021-05-16 00:10:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,17 +13,17 @@ def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
             return redirect(request.url)
-        files = request.files.getlist("file[]")
+        files = request.files.getlist("images")
         if not files:
             return
         
         results=[]
         for file in files:
-            img_bytes = file.read()
+            img_byte = file.read()
             class_id, class_name = get_prediction(image_byte=img_byte)
             class_name = format_class_name(class_name)
             results.append(class_name)
-        return render_template('result.html', class_name=results)
+        return render_template('result.html', class_name=class_name)
     return render_template('index.html')
 
 
