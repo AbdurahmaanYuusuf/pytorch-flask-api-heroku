328e5d1ad35c64bc33cf1ae0cd5f3384aaef3fa1
AbdurahmaanYuusuf <55019409+AbdurahmaanYuusuf@users.noreply.github.com>
2021-05-16 12:56:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 from flask import Flask, render_template, request, redirect
 
@@ -13,17 +14,18 @@ def upload_file():
     if request.method == 'POST':
         if 'files[]' not in request.files:
             return redirect(request.url)
-        files = request.files.getlist('files[]')
-        if not files:
+        files_src = request.files.getlist('files[]')
+        if not files_src:
             return
+        files_dst='/uploads'
+        files = shutil.copytree(files_src, files_dst, copy_function = shutil.copy)
         filenames=[]
         for file in files:
             file_name=file.filename
             filenames.append(file_name)
         results=[]
         for file in files:
-            img_byte = file.read()
-            class_id, class_name = get_prediction(image_byte=img_byte)
+            class_id, class_name = get_prediction(file)
             class_name = format_class_name(class_name)
             results.append(class_name)
         return render_template('result.html', results=results,files=filenames)
